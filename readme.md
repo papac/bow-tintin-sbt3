0e1eaab1e7b8eca611ad37a54993b5e790ecd95e
Franck Dakia <dakiafranck@gmail.com>
2018-10-16 10:16:14 +0000

    update readme

---
+++ readme.md
@@ -0,0 +1,35 @@
+## Tintin Highlighter
+
+This package adds syntax definitions for Bow Tintin engine (https://github.com/bowphp/tintin).
+
+*Works with Sublime Text 3, for older versions use an older release.*
+
+### How to install w/[Sublime Package Control](http://wbond.net/sublime_packages/package_control)
+
+1. Search for Bow Tintin and install it.
+2. Restart Sublime Text.
+3. Reopen any ```.tintin``` files.
+4. Enjoy :)
+
+### Sublime Text Manual Install
+
+1. Download or clone this repository into ```[install-dir]/Packages/bow-tintin```
+2. Restart Sublime Text.
+3. Reopen any ```.tintin``` files.
+4. Enjoy :)
+
+### Yeah but, show me what it is?
+
+![blade-example](https://cloud.githubusercontent.com/assets/499192/8564960/52a7e57c-2551-11e5-8182-1f24a6d8d17a.jpg "tintin-example")
+
+*[Predawn](https://github.com/jamiewilson/predawn).*
+
+#### How to Contribute
+
+* To test a local version of the highlighter first uninstall the highlighter from package control.
+* Follow the manual installation process by cloning the repo into your packages directory.
+* Restart Sublime Text.
+* Open up the '[install-dir]/Packages/bow-tintin' folder into a new Sublime Text project.
+* Open up the blade.tmLanguage file and make changes.
+* I have provided a test.blade file that holds most of the common uses for testing the regex, use this to verify your changes before and after you make them to ensure the changes you make do not break anything.
+* Send a pull request with a single change per request.
--- test.tintin.php
+++ test.tintin.php
@@ -6,12 +6,6 @@ The current UNIX timestamp is {{ time() }}.
 {{ isset($name) ? $name : 'Default' }}
 {{ $name or 'Default' }}
 
-{# Displaying Raw Text With Curly Braces #}
-#{{ This will not be processed by Blade }}
-
-{# Do not escape data #}
-Hello, {!! $name !!}.
-
 {# Escape Data #}
 Hello, {{{ $name }}}.
 
@@ -19,9 +13,9 @@ Hello, {{{ $name }}}.
 <?= $name; ?>
 
 <?php
-    foreach (range(1, 10) as $number) {
-        echo $number;
-    }
+foreach (range(1, 10) as $number) {
+  echo $number;
+}
 ?>
 
 #include('header')
@@ -30,47 +24,47 @@ Hello, {{{ $name }}}.
 
 {# PHP open/close tags #}
 <div class="container">
-    #php
-        foreach (range(1, 10) as $number) {
-            echo $number;
-        }
-    #endphp
+  #php
+    foreach (range(1, 10) as $number) {
+      echo $number;
+    }
+  #endphp
 </div>
 
 {# Inline PHP #}
 <div class="container">
-    #php(custom_function())
+  #php(custom_function())
 </div>
 
 #include('footer')
 
 {# Define Blade Layout #}
 <html>
-    <head>
-        <title>
-            #inject('title') - App Name
-        </title>
-    </head>
-    <body>
-        #block('sidebar')
-            This is the master sidebar.
-        #endblock
+<head>
+  <title>
+    #inject('title') - App Name
+  </title>
+</head>
+<body>
+  #block('sidebar')
+    This is the master sidebar.
+  #endblock
 
-        <div class="container">
-            #include('content')
-        </div>
-    </body>
+  <div class="container">
+    #include('content')
+  </div>
+</body>
 </html>
 
 {# Use Blade Layout #}
 #extends('layouts.master')
 
 #block('sidebar')
-    <p>This is appended to the master sidebar.</p>
+  <p>This is appended to the master sidebar.</p>
 #endblock
 
 #block('content')
-    <p>This is my body content.</p>
+  <p>This is my body content.</p>
 #endblock
 
 {# yield section #}
@@ -78,46 +72,46 @@ Hello, {{{ $name }}}.
 
 {# If Statement #}
 #if (count($records) === 1)
-    I have one record!
+  I have one record!
 #elseif (count($records) > 1)
-    I have multiple records!
+  I have multiple records!
 #elif (count($records) > 1)
-    I have multiple records!
+  I have multiple records!
 #else
-    I don't have any records!
+  I don't have any records!
 #endif
 
 <ul class="list #if (count($records) === 1) extra-class #endif">
-    <li>This is the first item</li>
-    <li>This is the second item</li>
+  <li>This is the first item</li>
+  <li>This is the second item</li>
 </ul>
 
 #isset($name)
-    Hello, {{ $name }}.
+  Hello, {{ $name }}.
 #endisset
 
 #unless (Auth::check())
-    You are not signed in.
+  You are not signed in.
 #endunless
 
 {# Loops #}
 #for ($i = 0; $i < 10; $i++)
-    The current value is {{ $i }}
+  The current value is {{ $i }}
 #endfor
 
 #loop ($users as $user)
-    <p>This is user {{ $user->id }}</p>
+  <p>This is user {{ $user->id }}</p>
 #endloop
 
 #while (true)
-    <p>I'm looping forever.</p>
+  <p>I'm looping forever.</p>
 #endwhile
 
 {# Single line if statement #}
 #if($foo === true) <p>Text</p> #endif
 
 {# Quoted blade directive matching #}
-<p class="first-class #if($x==true) second-class #endif">Text</p>
+<p class="first-class #if ($x == true) second-class #endif">Text</p>
 
 {# Complex conditional inline #}
 <p class="first-class #if(($x == true) && ($y == "yes")) second-class #endif">Text</p>
